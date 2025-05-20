3c5c31d4dea57fb10812372ab0775e5488affde2
Imane EL ALJI <i.elalji@eurafric.com>
2025-05-20 14:55:44 +0200

    Premier commit : projet initial

---
+++ src/main/java/com/example/e_sante_app/ESanteAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_sante_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESanteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESanteAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e_sante_app/ESanteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_sante_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESanteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
